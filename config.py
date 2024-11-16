5755057f5ea3f9da645f661ce7c4f6fdaf269e20
hemant67890 <godarasubhash337@gmail.com>
2024-11-16 20:53:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "24935727"))
 API_HASH = getenv("API_HASH", "3fd33336629324ecd664e9b6894f0909")
-BOT_TOKEN = getenv("BOT_TOKEN", "7609700133:AAG6TqHDZt3Mdi3Za5XKaP-xfcu0HFjwxTA")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "7336971189").split()))
+BOT_TOKEN = getenv("BOT_TOKEN", "7968743365:AAHvq_JsnLeDWLyB8ah2N0P3Wmw6jHFiYLY")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6326227068").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://sujay5372192:sujay5372192@cluster00001.zivqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00001")
-LOG_GROUP = getenv("LOG_GROUP", "-1002262545667")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002483720229"))
+LOG_GROUP = getenv("LOG_GROUP", "-1002369795937")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002397574322"))
