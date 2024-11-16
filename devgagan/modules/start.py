569c8463ce624be37ead0951a11e557ebd62681e
hemant67890 <godarasubhash337@gmail.com>
2024-11-16 21:36:19 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -19,6 +19,6 @@ async def start(_, message):
     join = await subscribe(_, message)
     if join == 1:
         return
-    await message.reply_photo(photo="https://graph.org/file/4e80dc2f4f6f2ddadb4d2.jpg",
+    await message.reply_photo(photo="https://images.app.goo.gl/KA14FzkZk3aCTaUi8",
                               caption=script.START_TXT.format(message.from_user.mention), 
                               reply_markup=buttons)
